46e41f369f857c11d3323d609764e290adc4aba6
2019-Q2_SOW_DEV_036 <2019Q2SowDEV036@gmail.com>
2019-07-10 00:06:39 +0200

    Extract and refactor the left minutes for single minutes row

--- src/BerlinClock.js
+++ src/BerlinClock.js
@@ -1,15 +1,28 @@
+var CONSTANTS = require('./config/Constants');
+
 var BerlinClock = function (time) {
+
     this.bottomMinutes = function () {
 
-        if (time.getMinutes() % 5 === 1) {
-            return "YOOO";
-        } else if (time.getMinutes() % 5 === 2) {
-            return "YYOO";
-        } else if (time.getMinutes() % 5 === 4) {
-            return "YYYY";
-        } else {
-            return "OOOO";
+        var illuminatedLamps, non_illuminatedLamps;
+
+        illuminatedLamps = getMinutesForSingleMinuteRow();
+
+        non_illuminatedLamps = CONSTANTS.TOTAL_BOTTOM_MINUTES_LAMP - illuminatedLamps;
+
+        return signLamps(illuminatedLamps, CONSTANTS.YELLOW) + signLamps(non_illuminatedLamps, CONSTANTS.OFF);
+    };
+
+    function getMinutesForSingleMinuteRow() {
+        return time.getMinutes() % CONSTANTS.TIME_PER_LAMP_MINUTES;
+    }
+
+    function signLamps(numberLamps, lampSign) {
+        var result = "";
+        for (var lamp = 0; lamp < numberLamps; lamp++) {
+            result += lampSign;
         }
+        return result;
     }
 };
 module.exports = BerlinClock;
---
+++ src/config/Constants.js
@@ -0,0 +1,7 @@
+var CONSTANTS = {
+    TOTAL_BOTTOM_MINUTES_LAMP: "4",
+    TIME_PER_LAMP_MINUTES: "5",
+    YELLOW: "Y",
+    OFF: "O"
+};
+module.exports = CONSTANTS;
